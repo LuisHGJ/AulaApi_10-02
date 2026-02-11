e46b8ef5c4540f86455ffe0560b05ea80c624a95
Luis Henrique <110108496+LuisHGJ@users.noreply.github.com>
2026-02-11 19:11:22 +0000

    aula1002

---
+++ src/main/java/app/controllers/TarefasController.java
@@ -0,0 +1,24 @@
+package app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import app.model.Tarefa;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefasController {
+    @GetMapping
+    public Tarefa list() {
+        return new Tarefa(1, "Teste");
+    }
+
+    @PostMapping
+    public Tarefa insert(@RequestBody Tarefa nova) {
+        return nova;
+    }
+}
